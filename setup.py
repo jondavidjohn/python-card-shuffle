14249ddaceb5dacbc99483c7a4fe31ac5674795a
Jonathan Johnson <me@jondavidjohn.com>
2012-01-25 12:33:08 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup, Extension
 
 module = Extension('shuffle', sources=['shufflemodule.c'])
-setup(name='Card Shuffling', version='1.0', ext_modules=[module])
+setup(name='Card Shuffling for Python Lists', version='1.0', ext_modules=[module])
